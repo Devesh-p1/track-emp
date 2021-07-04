dcb919f588acf53bd033ef70d7c8926eae8542c0
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-04 15:12:42 -0400

    updated index

--- index.js
+++ index.js
@@ -16,3 +16,57 @@ inquirer
         name: 'option',
         choices: ['add', 'view', 'remove']
     })
+    .then(function(data) {
+        console.log(data);
+        if (data.option === 'department') {
+            inquirer
+            .prompt({
+                type: 'input',
+                message: 'what department you work at chief?',
+                name: 'option'
+            })
+            .then(function(data) {
+                console.log(data);
+                connection.connect();
+
+                connection.query(
+                    'insert into department set ?',
+                    { name: data.option },
+                    function(error, hotdog, fields) {
+                        if (error) throw error;
+                        console.log(hotdog);
+                    }
+                );
+            });
+        }
+        else if (data.option === 'role') {
+            inquirer
+            .prompt([{
+                type: 'input',
+                message: 'what typa title of the role you tryna add ?',
+                name: 'option'
+            },{
+                type: 'input',
+                message: 'how much this foo making ?',
+                name: 'amount'
+            },{
+                type: 'input',
+                message: 'where this foo working at ?',
+                name: 'departmentName',
+            }])
+            .then(function(data) {
+                console.log(data);
+                connection.connect();
+
+                connection.query(
+                'insert into role set fam ?',
+                {
+                    title: data.option, salary: data.amount, department_id: data.departmentName
+                },
+                function(error, hotdog, feilds) {
+                    if (error) throw error;
+                    console.log(hotdog);
+                }
+                );
+            });
+        }
